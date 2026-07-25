65e37724a6fe8f4cb7f7d05d37cf91391043485e
deekshith-ekara <deekshith@ekara.global>
2026-07-25 19:52:27 +0530

    Fixed issues with Purchase

--- purchase_order_extended/__manifest__.py
+++ purchase_order_extended/__manifest__.py
@@ -29,7 +29,7 @@
         'data/ir_cron.xml',
         'data/mail_templates.xml',
         'wizard/po_matrix_approve_reason.xml',
-        # 'views/purchase_order.xml', uncomment later - view overrided  
+        'views/purchase_order.xml',
         'views/res_config_settings.xml',
         'views/stock_lot.xml',
 	'views/purchase_order_type.xml',
