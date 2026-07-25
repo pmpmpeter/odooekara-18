e5eb4c2ab44dc6ee730336ce33810839c8ad500e
deekshith-ekara <deekshith@ekara.global>
2026-07-25 19:52:52 +0530

    Resolved issues with Leave

--- oi_hr_holidays_attachment/models/hr_leave.py
+++ oi_hr_holidays_attachment/models/hr_leave.py
@@ -13,13 +13,13 @@ class HolidaysRequest(models.Model):
         ('am', 'Session 1'), ('pm', 'Session 2')],
         string="Date Period Start", default='am')
 
-    @api.constrains('state','holiday_status_id', 'number_of_days_display')
+    @api.constrains('state','holiday_status_id', 'number_of_days')
     def _check_attachment(self):
         if self.env.context.get('leave_skip_attachment_check', False):
             return
 
         for record in self:
-            if record.state not in ['draft', 'cancel', 'refuse'] and record.holiday_status_id.attachment_required and record.number_of_days_display > 3:
+            if record.state not in ['draft', 'cancel', 'refuse'] and record.holiday_status_id.attachment_required and record.number_of_days > 3:
                 if not self.env['ir.attachment'].search([('res_model','=', self._name), ('res_id','=', record.id)], limit = 1):
                     raise ValidationError(_('You cannot send the leave request without attaching a document.'))
 
